878ea5150e904de9e204e00a55f4ea08f210e188
Niklas B <68842909+justSalkin@users.noreply.github.com>
2023-06-13 09:40:23 +0200

    Add files via upload

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Krieger I Re/Say (2)</name>
+        <message>
+            <source>Gut gemacht! Virabhadrasana eins ist eine großartige Übung, um die Beine, Arme und Schultern zu stärken und die Körperhaltung zu verbessern. Wir machen weiter mit Virabhadrasana zwei, bekannt als Krieger zwei.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Gut gemacht! Virabhadrasana eins ist eine großartige Übung, um die Beine, Arme und Schultern zu stärken und die Körperhaltung zu verbessern. Wir machen weiter mit Virabhadrasana zwei, bekannt als Krieger zwei.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Gut gemacht! Virabhadrasana eins ist eine gute Übung, um die Beine, Arme und Schultern zu stärken und die Körperhaltung zu verbessern. Wir machen weiter mit Virabhadrasana zwei, bekannt als Krieger zwei.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Gut gemacht! Virabhadrasana eins ist eine gute Übung, um die Beine, Arme und Schultern zu stärken und die Körperhaltung zu verbessern. Wir machen weiter mit Virabhadrasana zwei, bekannt als Krieger zwei.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Outro/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bis zum nächsten Mal!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bis zum nächsten Mal!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Wir gehen weiter in die zweite Position.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wir gehen weiter in die zweite Position.</translation>
+        </message>
+        <message>
+            <source>Gut gemacht! Tadasana ist eine wirksame Übung, um eure Körperhaltung und Entspannung zu verbessern. Wir machen weiter mit Virabhadrasana eins, bekannt als Krieger eins.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Gut gemacht! Tadasana ist eine wirksame Übung, um eure Körperhaltung und Entspannung zu verbessern. Wir machen weiter mit Virabhadrasana eins, bekannt als Krieger eins.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Sehr gut! Wir wechseln nun die Seiten und starten wieder in unserer Bergposition.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Sehr gut! Wir wechseln nun die Seiten und starten wieder in unserer Bergposition.</translation>
+        </message>
+        <message>
+            <source>Jetzt kommen wir zurück zur Bergposition und dann wechseln wir die Seiten.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Jetzt kommen wir zurück zur Bergposition und dann wechseln wir die Seiten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Gut gemacht! Virabhadrasana eins ist eine großartige Übung, um die Beine, Arme und Schultern zu stärken und die Körperhaltung zu verbessern. Wir machen weiter mit Virabhadrasana zwei, bekannt als Krieger zwei.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Gut gemacht! Virabhadrasana eins ist eine großartige Übung, um die Beine, Arme und Schultern zu stärken und die Körperhaltung zu verbessern. Wir machen weiter mit Virabhadrasana zwei, bekannt als Krieger zwei.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Tadasana/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Gut gemacht! Tadasana ist eine wirksame Übung, um eure Körperhaltung und Entspannung zu verbessern. Wir machen weiter mit Virabhadrasana eins, bekannt als Krieger eins.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Gut gemacht! Tadasana ist eine wirksame Übung, um eure Körperhaltung und Entspannung zu verbessern. Wir machen weiter mit Virabhadrasana eins, bekannt als Krieger eins.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Jetzt kommen wir zurück zur Bergposition und dann wechseln wir die Seiten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jetzt kommen wir zurück zur Bergposition und dann wechseln wir die Seiten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Warrior I Rechts/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/one leg/Say</name>
+        <message>
+            <source>stand on one leg for 3 sec.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">stand on one leg for 3 sec.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/one leg/praise</name>
+        <message>
+            <source>
+You're really good at balancing.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">
+You're really good at balancing.</translation>
+        </message>
+    </context>
+</TS>
